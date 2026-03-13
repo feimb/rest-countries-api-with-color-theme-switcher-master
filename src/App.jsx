56aa5ea22b18be3105b4c448d4d0affe2689319a
feichulin <feimosqueda777@gmail.com>
2026-03-12 22:26:44 -0300

    Iniciando el proyecto con todas sus dependencias y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div className="bg-blue-900 text-white p-10">Test Tailwind</div>
+        </>
+    );
+}
+
+export default App;
